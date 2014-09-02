7df17cad607f5a9297ff4817839405ed06b81ce3
Xiangyan Sun <wishstudio@gmail.com>
2014-09-02 17:01:57 +0800

    Fix sys_exec() prototype.

--- src/syscall/exec.c
+++ src/syscall/exec.c
@@ -221,7 +221,7 @@ int do_execve(const char *filename, int argc, char *argv[], int env_size, char *
 
 static char *const startup = (char *)STARTUP_DATA_BASE;
 
-int sys_execve(const char *filename, char *argv[], char *envp[], int _4, int _5, PCONTEXT context)
+int sys_execve(const char *filename, char *argv[], char *envp[], int _4, int _5, int _6, PCONTEXT context)
 {
 	/* TODO: Deal with argv/envp == NULL */
 	/* TODO: Don't destroy things on failure */
--- src/syscall/exec.h
+++ src/syscall/exec.h
@@ -4,4 +4,4 @@
 
 int do_execve(const char *filename, int argc, char *argv[], int env_size, char *envp[], PCONTEXT context);
 
-int sys_execve(const char *filename, char *argv[], char *envp[], int _4, int _5, PCONTEXT context);
+int sys_execve(const char *filename, char *argv[], char *envp[], int _4, int _5, int _6, PCONTEXT context);
