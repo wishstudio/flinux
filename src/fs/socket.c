f001e5938edd727c48df5ec4948568cf498af0ee
Xiangyan Sun <wishstudio@gmail.com>
2015-03-31 03:41:26 +0800

    socket: Add sockopt SO_REUSEADDR. netcat works.

--- src/fs/socket.c
+++ src/fs/socket.c
@@ -809,6 +809,9 @@ static int socket_get_set_sockopt(int call, struct socket_file *f, int level, in
 		level = SOL_SOCKET;
 		switch (optname)
 		{
+		case LINUX_SO_REUSEADDR: optname = SO_REUSEADDR;
+			log_warning("SO_REUSEADDR: Current semantic is not exactly correct.\n");
+			goto get_set_sockopt;
 		case LINUX_SO_ERROR: optname = SO_ERROR; goto get_set_sockopt;
 		case LINUX_SO_BROADCAST: optname = SO_BROADCAST; goto get_set_sockopt;
 		case LINUX_SO_SNDBUF: optname = SO_SNDBUF; goto get_set_sockopt;
